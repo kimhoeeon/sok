6516af8c8ab39c4b974aaf259a54b9ff1e643812
KHE <khe@meetingfan.com>
2026-08-20 15:04:49 +0900

    2026.08.20 : 프로젝트 커밋

--- src/main/java/org/mtf/sok/config/SecurityConfig.java
+++ src/main/java/org/mtf/sok/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -39,7 +38,7 @@ public class SecurityConfig {
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
                 .authorizeRequests()
-                .antMatchers("/mng/login", "/common/loginProc", "/css/**", "/js/**", "/images/**", "/img/**").permitAll()
+                .antMatchers("/mng/login", "/common/loginProc", "/css/**", "/js/**", "/images/**", "/img/**", "/.well-known/**", "/sitemap.xml").permitAll()
                 .antMatchers("/mng/**").hasAnyRole("ADMIN", "DEVELOPER")
                 .antMatchers("/mypage/**").authenticated()
                 .anyRequest().permitAll()
@@ -165,10 +164,5 @@ public class SecurityConfig {
                 response.sendRedirect("/login/basic?error=true&exception=" + encodedMsg);
             }
         };
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("/.well-known/**", "/sitemap.xml");
     }
 }
